2a710d97747cf5d4d93ea27cf09418103bfc86e8
eluemnaomi <eluemnaomi4@gmail.com>
2023-09-13 21:04:26 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -5,14 +5,9 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-int r;
-
-
-print_last_digit(0);
-r = print_last_digit(-1024);
-_putchar('0' + r);
-_putchar('\n');
+jack_bauer();
 return (0);
 }
